67475309217b071a12db090b765ec6c95bdc0fb3
shubhramishrakota2310 <shubhramishrakota2310@gmail.com>
2021-03-31 17:06:58 +0530

    hashrouter added to index.js

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import {BrowserRouter} from 'react-router-dom';
+import {HashRouter} from 'react-router-dom';
 import reportWebVitals from '../src/components/Project/reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 
 ReactDOM.render(
-  <BrowserRouter>
+  <HashRouter>
     <App/>
-  </BrowserRouter>,
+  </HashRouter>,
   document.getElementById('root')
 );
 
